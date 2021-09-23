97e475f298f1d35483a6559ad7d4421bb34e4bc9
Debora Souza <deboral.p@hotmail.com>
2021-09-23 12:11:44 -0300

    Classe herois com atributos-metodo construtor e get e set

--- src/br/com/zup/Heroi.java
+++ src/br/com/zup/Heroi.java
@@ -1,4 +1,27 @@
 package br.com.zup;
 
 public class Heroi {
+    private String nome;
+    private double vida;
+
+    public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
 }
