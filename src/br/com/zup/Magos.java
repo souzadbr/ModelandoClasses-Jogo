2b0c13cb2cc81c3d53aa0e7c799ffb05dd97f5d7
Debora Souza <deboral.p@hotmail.com>
2021-09-23 13:23:16 -0300

    Classe Mago com atributos, metodo construtor e metodo + extendo Heroi

--- src/br/com/zup/Magos.java
+++ src/br/com/zup/Magos.java
@@ -1,4 +1,22 @@
 package br.com.zup;
 
-public class Magos {
+public class Magos extends Heroi {
+    private double poderMagico;
+
+    public Magos(String nome, double vida, double poderMagico) {
+        super(nome, vida);
+        this.poderMagico = poderMagico;
+    }
+
+    public double getPoderMagico() {
+        return poderMagico;
+    }
+
+    public void setPoderMagico(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
+
+    public double aplicarMagia(double magia){
+
+    }
 }
