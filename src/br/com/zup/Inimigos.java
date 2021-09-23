8ce04d46cb2059f79498deb5ea2fd65aa2d2f5cd
Debora Souza <deboral.p@hotmail.com>
2021-09-23 13:12:47 -0300

    Atributos, Metodo construtor e get e set da classe inimigos

--- src/br/com/zup/Inimigos.java
+++ src/br/com/zup/Inimigos.java
@@ -1,4 +1,27 @@
 package br.com.zup;
 
 public class Inimigos {
+    private String nome;
+    private double vida;
+
+    public Inimigos(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
 }
