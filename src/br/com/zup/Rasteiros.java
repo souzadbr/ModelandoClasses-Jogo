7e904806ecd132fd4421bd8e0651a18774518802
Debora Souza <deboral.p@hotmail.com>
2021-09-23 13:38:10 -0300

    classe Rastreiros extends Inimigos com atributos, metodos construtor e metodos

--- src/br/com/zup/Rasteiros.java
+++ src/br/com/zup/Rasteiros.java
@@ -1,4 +1,22 @@
 package br.com.zup;
 
-public class Rasteiros {
+public class Rasteiros extends Inimigos {
+    private double danoEspinho;
+
+    public Rasteiros(String nome, double vida, double danoEspinho) {
+        super(nome, vida);
+        this.danoEspinho = danoEspinho;
+    }
+
+    public double getDanoEspinho() {
+        return danoEspinho;
+    }
+
+    public void setDanoEspinho(double danoEspinho) {
+        this.danoEspinho = danoEspinho;
+    }
+
+    public double aplicarDano(double dano){
+
+    }
 }
